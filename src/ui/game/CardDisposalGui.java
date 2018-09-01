7f8132782c3edeeb32fdfd5150f1b60564eb436d
harry <zeyuzeng@alumni.cmu.edu>
2018-09-01 14:55:52 -0700

    Card disposal now has one single row instead

--- src/ui/game/CardDisposalGui.java
+++ src/ui/game/CardDisposalGui.java
@@ -6,8 +6,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-import listeners.game.CardDisposalListener;
 import cards.Card;
+import listeners.game.CardDisposalListener;
 
 /**
  * Gui of card disposal area, public to all players
@@ -20,9 +20,8 @@ public class CardDisposalGui extends JPanel implements CardDisposalListener, Act
 	private static final long serialVersionUID = 1L;
 	
 	public static final int WIDTH = 600;
-	public static final int HEIGHT = CardGui.HEIGHT * 2;
+	public static final int HEIGHT = CardGui.HEIGHT;
 	private static final int TIME = 50;
-	private CardRackGui disposal;
 	private CardRackGui usage;
 	private Timer timer;
 	private int ms;
@@ -38,10 +37,6 @@ public class CardDisposalGui extends JPanel implements CardDisposalListener, Act
 		usage.setLocation(0, 0);
 		add(usage);
 
-		disposal = new CardRackGui(null);
-		disposal.setSize(WIDTH, CardGui.HEIGHT);
-		disposal.setLocation(0, CardGui.HEIGHT);
-		add(disposal);
 		timer = new Timer(10, this);
 		ms = 0;
 	}
@@ -55,14 +50,14 @@ public class CardDisposalGui extends JPanel implements CardDisposalListener, Act
 
 	@Override
 	public void onCardDisposed(Card card) {
-		disposal.onCardAdded(card);
+		usage.onCardAdded(card);
 		parent.validate();
 		parent.repaint();
 	}
 	
 	@Override
 	public void onCardShown(Card card) {
-		disposal.onCardAdded(card);
+		usage.onCardAdded(card);
 		parent.validate();
 		parent.repaint();
 	}
@@ -76,14 +71,12 @@ public class CardDisposalGui extends JPanel implements CardDisposalListener, Act
 	public void actionPerformed(ActionEvent e) {
 		if (ms == 2 * TIME) {
 			usage.clearRack();
-			disposal.clearRack();
 			timer.stop();
 			ms = 0;
 		} else {
 			ms++;
 			if (ms > TIME) {
 				usage.fadeCards(1.0f / TIME);
-				disposal.fadeCards(1.0f / TIME);
 			}
 		}
 	}
--- src/ui/game/MessageBoxGui.java
+++ src/ui/game/MessageBoxGui.java
@@ -1,30 +1,26 @@
 package ui.game;
 
 import java.awt.Font;
+
 import javax.swing.JLabel;
 
-public class MessageBoxGui extends JLabel
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1741141955211814971L;
+public class MessageBoxGui extends JLabel {
+	private static final long serialVersionUID = 1L;
+	
 	public static final int WIDTH = CardRackGui.WIDTH;
 	public static final int HEIGHT = 40;
-	
-	public MessageBoxGui()
-	{
+
+	public MessageBoxGui() {
 		setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
 		setHorizontalAlignment(JLabel.CENTER);
-		setSize(WIDTH,HEIGHT);
+		setSize(WIDTH, HEIGHT);
 	}
-	
-	public void setMessage(String message)
-	{
+
+	public void setMessage(String message) {
 		setText(message);
 	}
-	public void clearMessage()
-	{
+
+	public void clearMessage() {
 		setText("");
 	}
 }
